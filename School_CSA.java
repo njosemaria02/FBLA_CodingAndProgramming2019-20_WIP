0ea8f6d7719976d9baa41b79db6cf51fb6273002
Nathaly Jose-Maria <njosemaria02@gmail.com>
2019-11-25 12:36:59 -0500

    Add files via upload
    FINALLY figured out how to link text file database with application. Also created more pages.

--- School_CSA.java
+++ School_CSA.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class School_CSA {
@@ -6,6 +9,15 @@ public class School_CSA {
 	public School_CSA()
 	{
 		studentList = new ArrayList<Student>();
+	}
+	
+	public School_CSA(ArrayList temp)
+	{
+		studentList = new ArrayList<Student>();
+
+		for (int len = 0; len < temp.size(); len++)
+			addStudent((Student) temp.get(len));
+		
 	}
 	
 	public void addStudent(String firstName, String lastName, String ID, String grade, String totalServiceHours)
@@ -20,6 +32,12 @@ public class School_CSA {
 	public void addStudent(Student student)
 	{
 		studentList.add(student);
+	}
+	
+	public boolean removeStudent(int index)
+	{
+		Student temp = studentList.get(index);
+		return (studentList.remove(index) == temp);
 	}
 	
 	public int getArrayLength()
@@ -37,14 +55,29 @@ public class School_CSA {
 		return studentList.get(index);
 	}
 	
+	public void updateDatabase(String fn)
+	{
+		try 
+		{ 
+			BufferedWriter out = new BufferedWriter(new FileWriter(fn)); 
+				out.write(toString()); 
+				out.close(); 
+		} 
+		catch (IOException e1) 
+		{ 
+			System.out.println("Exception Occurred" + e1); 
+		} 
+	}
+	
 	public String toString()
 	{	
-		String result = "";
+		String result = "Alpharetta High School Roster";
 		
 		for (int i = 0; i < studentList.size(); i++)
 		{
-			result += "\n\nNo. " + (i+1) + " Alpharetta High School Student ------------------------\n"; // 24 dashes
+			result += "\nNo. " + (i+1) + " Alpharetta High School Student ------------------------\n"; // 24 dashes
 			result += (studentList.get(i));
+			result += "\n---";
 		}
 		
 		return result;
